08db5f986dd93e35572f1e0d7ee9962b6539dafa
Stas Smok <kylbitskiy2004@mail.ru>
2023-11-21 23:30:14 +0300

    Added obsticles

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import pygame as pg, math
+from utils import *
 from config import *
 from scene import Scene
 from pypongengine import PyPongEngine
 
 from objects.world_borders import WorldBorders
 from objects.player import Player
+from objects.obstacle import Obstacle
 
 class ExampleScene(Scene):
     def reset(self, engine) -> None:
@@ -14,6 +16,13 @@ class ExampleScene(Scene):
             Player(scene=self),
             WorldBorders(scene=self)
         ]
+        y = 9.8
+        while y > 0:
+            x = -8.75
+            while x < 9:
+                self.objects.append(Obstacle(scene = self, position=[x,y]))
+                x += 0.5
+            y -= 0.45
 
 if __name__=="__main__":
     engine = PyPongEngine()
