0577294170f7cec2a18983cd64e06ed34fa1e6f0
filebin <alex.mousse9@gmail.com>
2023-11-30 02:47:48 +0300

    physics fixed (most of all)

--- objects/ball.py
+++ objects/ball.py
@@ -31,7 +31,6 @@ class Ball(GameObject):
         else:
             ball_radius = max(self.scale) * 0.5
             colliders = [x for x in self.scene.objects if x.active and x.enable_collision]
-            step_distance = self.speed * dt
             
             def sdf_o(p: vec2):
                 return min(list((c.sdf(p), c) for c in colliders), key=lambda t: t[0])
@@ -39,58 +38,71 @@ class Ball(GameObject):
             def sdf(p: vec2):
                 return sdf_o(p)[0]
             
-            pos = self.position
+            def move(step_distance):
+                pos = self.position
 
-            direction = normalize(self.velocity)
+                direction = normalize(self.velocity)
 
-            collision = False
+                collision = False
 
-            dist, nearestObject = sdf_o(pos)
-            while(dist - ball_radius < step_distance):
-                if(dist > ball_radius):
+                dist, nearestObject = sdf_o(pos)
 
-                    phys_step = max(ball_radius*0.5, dist - ball_radius)
-                    pos = list(map(add, pos, vec2(n*phys_step for n in direction)))
-                    dist, nearestObject = sdf_o(pos)
-                    step_distance -= phys_step
-                else:
-                    collision = True 
-                    break
-
-            if(collision): # collision happens
-                self.position = pos
-                normal = list(normal_sdf(sdf, self.position))
-
-                # just for that type of game limit amount of normals to avoid bugs
-                if all(abs(x) > 0.01 for x in normal):
-                    normals = [list(normal), list(normal)]
-                    normals[0][0] = 0
-                    normals[1][1] = 0
-                    normals = [normalize(x) for x in normals]
-                    if dot(normals[0], direction) < dot(normals[1], direction):
-                        normal = normals[0]
+                while(dist - ball_radius < step_distance):
+                    if(dist > ball_radius):
+                        phys_step = max(0.01, dist - ball_radius)
+                        pos = list(map(add, pos, vec2(n*phys_step for n in direction)))
+                        dist, nearestObject = sdf_o(pos)
+                        step_distance -= phys_step
                     else:
-                        normal = normals[1]
-                elif abs(normal[0]) > abs(normal[1]):
-                    normal[1] = 0
-                else:
-                    normal[0] = 0
-                
-                normal = normalize(normal)
-                if(nearestObject.tag == PLAYER_TAG):
-                    if(normal[1] >= 0):
-                        self.calculate_velocity_from_player()
-                else:
-                    if nearestObject.tag == OBSTACLE_TAG:
-                        nearestObject.active = False
-                    elif nearestObject.tag == WORLDBORDER_TAG:
-                        if(normal[1] > 0.5):
-                            # ball hit bottom wall
-                            from scenes.gamescene import GameScene 
-                            self.scene.engine.change_scene(GameScene())
-                    if dot(self.velocity, normal) < 0:
-                        self.velocity = list(reflect(self.velocity, normal))
-            self.position = [x + y * dt for x, y in zip(self.position, self.velocity)]                  
+                        collision = True 
+                        break
+
+                if not collision:
+                    step_distance = self.speed * dt
+                    self.position = [x + y * step_distance for x, y in zip(self.position, direction)]
+                    return
+                if(collision): # collision happens
+                    self.position = pos
+                    normal = list(normal_sdf(sdf, self.position))
+
+                    # just for that type of game limit amount of normals to avoid bugs
+                    if all(abs(x) > 0.3 for x in normal):
+                        normals = [list(normal), list(normal)]
+                        normals[0][0] = 0
+                        normals[1][1] = 0
+                        normals = [normalize(x) for x in normals]
+                        if dot(normals[0], direction) < dot(normals[1], direction):
+                            normal = normals[0]
+                        else:
+                            normal = normals[1]
+                    elif abs(normal[0]) > abs(normal[1]):
+                        normal[1] = 0
+                    else:
+                        normal[0] = 0
+                    
+                    normal = normalize(normal)
+                    if(nearestObject.tag == PLAYER_TAG):
+                        if(normal[1] > -0.001):
+                            self.calculate_velocity_from_player()
+                    else:
+                        if nearestObject.tag == OBSTACLE_TAG:
+                            nearestObject.active = False
+                        elif nearestObject.tag == WORLDBORDER_TAG:
+                            if(normal[1] > 0.5):
+                                # ball hit bottom wall
+                                from scenes.gamescene import GameScene 
+                                self.scene.engine.change_scene(GameScene())
+                        if dot(self.velocity, normal) < 0:
+                            self.velocity = list(reflect(self.velocity, normal))
+                        direction = normalize(self.velocity)
+                self.position = [x + y * 0.01 for x, y in zip(self.position, direction)]
+                step_distance -= 0.01
+                if(step_distance>0.01):
+                    print(step_distance)
+                    move(step_distance)
+
+            move(self.speed * dt)
+              
 
     def calculate_velocity_from_player(self):
             target_position = list(self.player.position)
